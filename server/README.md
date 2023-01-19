83493498bd7e5ad848f86a5e8ac09602af301c70
Izaías Lima <izaiaslima356@gmail.com>
2023-01-18 22:19:18 -0300

    chore(server): complete prisma schema and seeds

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# HABIT TRACKER
+
+<div align="center">
+	<img src="https://i.imgur.com/ngoilkc.jpg"/>
+</div>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from '.prisma/client';
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 
 const prisma = new PrismaClient();
 
 const app = Fastify();
+
+app.register(cors);
 
 app.get('/habits', async () => {
   const habits = await prisma.habit.findMany();
