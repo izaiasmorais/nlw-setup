5f0bdb0d31de9491ae4e413c57c3a34e4e2a7aac
Izaías Lima <izaiaslima356@gmail.com>
2023-01-20 20:44:03 -0300

    chore(web): create header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from './assets/components/Header';
+
 function App() {
   return (
-    <div className="text-violet-500 w-full min-h-full">
-      <h1>Hello world</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   );
 }
