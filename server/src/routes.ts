9a46679fa3c94bd20636d6c0e64ee27dced49e32
Izaías Lima <izaiaslima356@gmail.com>
2023-01-21 21:32:07 -0300

    chore(server): complete habits toggle by id route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -68,4 +68,76 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabit,
     };
   });
+
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitsParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitsParams.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
+
+  app.get('/summary', async () => {
+    const summary = await prisma.$queryRaw`
+			SELECT
+				D.id,
+				D.date,
+				(
+					SELECT
+						cast(count(*) as float)
+					FROM day_habits DH
+					WHERE DH.day_id = D.id
+				) as completed,
+				(
+					SELECT
+						cast(count(*) as float)
+					FROM habit_week_days HWD
+					WHERE
+						HWD.week_day = 
+				) as amount
+			FROM day D
+		`;
+
+    return summary;
+  });
 }
