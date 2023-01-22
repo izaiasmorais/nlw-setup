492019d98c6ea731952b29d528ff521eb2b5db8d
Izaías Lima <izaiaslima356@gmail.com>
2023-01-21 21:38:21 -0300

    chore(server): complete summary route using sql

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -132,8 +132,11 @@ export async function appRoutes(app: FastifyInstance) {
 					SELECT
 						cast(count(*) as float)
 					FROM habit_week_days HWD
+					JOIN habits H
+						ON H.id = HWD.habit_id
 					WHERE
-						HWD.week_day = 
+						HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+						AND H.created_at <= D.date
 				) as amount
 			FROM day D
 		`;
